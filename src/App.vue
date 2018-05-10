97bbd652b045b3a5deeec84a8daff2adb6837f8c
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-05-10 11:31:46 +0200

    updated to latest vue-cli stack

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+
+  img {
+    background-color: lightblue;
+    border-radius: 50%;
+  }
+}
+</style>
